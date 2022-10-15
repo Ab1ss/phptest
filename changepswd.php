0943ac608661a3746f58c73739ad6d562f70fe8a
Ab1ss <abys.power@gmail.com>
2022-10-15 18:59:14 +0300

    добавление стилей к форме смена пароля

--- changepswd.php
+++ changepswd.php
@@ -4,11 +4,23 @@ if (!$_SESSION['user']) {
     header('Location: /');
 }
 ?>
-    <<form action="vendor/passchange.php" method="post">>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Смена пароля</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    <form action="vendor/passchange.php" method="post">
         <label>Введите старый пароль</label>
         <input type="password" name="old_password" placeholder="Введите старый пароль">
         <label>Введите новый пароль</label>
         <input type="password" name="new_password" placeholder="Введите новый пароль">
         <label>Введите новый пароль</label>
         <input type="password" name="new_password_confirm" placeholder="Введите новый пароль">
+        <button type="submit">Сменить пароль</button>
     </form>
+</body>
+</html>
