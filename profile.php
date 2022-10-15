8bf38ba1f174204b6123fed94043e68a9944f4e8
Ab1ss <abys.power@gmail.com>
2022-10-15 17:52:57 +0300

    password_hash and minor changes

--- profile.php
+++ profile.php
@@ -17,9 +17,8 @@ if (!$_SESSION['user']) {
     <!-- Профиль -->
 
     <form>
-        <img src="<?= $_SESSION['user']['avatar'] ?>" width="200" alt="">
-        <h2 style="margin: 10px 0;"><?= $_SESSION['user']['full_name'] ?></h2>
-        <a href="#"><?= $_SESSION['user']['email'] ?></a>
+        <h2 style="margin: 10px 0;"><?= $_SESSION['user']['login'] ?></h2>
+        <a href="#"><?= $_SESSION['user']['login'] ?></a>
         <a href="vendor/logout.php" class="logout">Выход</a>
     </form>
 
--- vendor/connect.php
+++ vendor/connect.php
@@ -1,7 +1,9 @@
 <?php
 
-    $connect = mysqli_connect('localhost', 'root', '', 'test');
+    $connect = mysqli_connect('127.0.0.1:3306', 'abyss', 'qwe123', 'test');
 
     if (!$connect) {
         die('Error connect to DataBase');
+
     }
+    ?>
