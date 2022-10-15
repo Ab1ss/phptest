f71cf3378a80b756bb351fd68e705343dd67c9df
Ab1ss <abys.power@gmail.com>
2022-10-15 18:30:52 +0300

    hashedpassword

--- vendor/signup.php
+++ vendor/signup.php
@@ -9,15 +9,9 @@
 
     if ($password === $password_confirm) {
 
-        $path = 'uploads/' . time() . $_FILES['avatar']['name'];
-        if (!move_uploaded_file($_FILES['avatar']['tmp_name'], '../' . $path)) {
-            $_SESSION['message'] = 'Ошибка при загрузке сообщения';
-            header('Location: ../register.php');
-        }
+        $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
 
-        $password = password_hash($password);
-
-        mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `password`) VALUES (NULL, '$login','$password')");
+        mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `password`) VALUES (NULL, '$login', '$hashedpassword')");
 
         $_SESSION['message'] = 'Регистрация прошла успешно!';
         header('Location: ../index.php');
