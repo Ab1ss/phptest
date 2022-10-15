f67016653db9dead3711c70e50b4007315492bf1
Ab1ss <abys.power@gmail.com>
2022-10-15 18:38:39 +0300

    проверка на существующий логин

--- vendor/signup.php
+++ vendor/signup.php
@@ -6,20 +6,19 @@
     $login = $_POST['login'];
     $password = $_POST['password'];
     $password_confirm = $_POST['password_confirm'];
-
-    if ($password === $password_confirm) {
-
-        $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
-
-        mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `password`) VALUES (NULL, '$login', '$hashedpassword')");
-
-        $_SESSION['message'] = 'Регистрация прошла успешно!';
-        header('Location: ../index.php');
-
-
+    $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
+    if (mysqli_num_rows($check_user) == 0) {
+        if ($password === $password_confirm) {
+            $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
+            mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `password`) VALUES (NULL, '$login', '$hashedpassword')");
+            $_SESSION['message'] = 'Регистрация прошла успешно!';
+            header('Location: ../index.php');
+        } else {
+            $_SESSION['message'] = 'Пароли не совпадают';
+            header('Location: ../register.php');
+        }
     } else {
-        $_SESSION['message'] = 'Пароли не совпадают';
+        $_SESSION['message'] = 'Логин уже существует';
         header('Location: ../register.php');
     }
-
 ?>
